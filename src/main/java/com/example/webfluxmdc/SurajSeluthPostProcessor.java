656684546d401ed53c2edd15e8f46bea2708291c
birbirs <sgrg007@gmail.com>
2022-05-16 07:42:30 -0500

    refactoring

--- src/main/java/com/example/webfluxmdc/SurajSeluthPostProcessor.java
+++ src/main/java/com/example/webfluxmdc/SurajSeluthPostProcessor.java
@@ -5,17 +5,16 @@ import org.springframework.boot.env.EnvironmentPostProcessor;
 import org.springframework.core.env.ConfigurableEnvironment;
 import org.springframework.core.env.MutablePropertySources;
 import org.springframework.core.env.PropertiesPropertySource;
-import org.springframework.stereotype.Component;
 
 import java.util.Properties;
 
 public class SurajSeluthPostProcessor implements EnvironmentPostProcessor {
     @Override
     public void postProcessEnvironment(ConfigurableEnvironment environment, SpringApplication application) {
-        Properties properties=new Properties();
-        properties.put("spring.sleuth.log.slf4j.whitelisted-mdc-keys","principal,suraj");
-        properties.put("spring.sleuth.baggage-keys","principal,suraj");
+        Properties properties = new Properties();
+        properties.put("spring.sleuth.log.slf4j.whitelisted-mdc-keys", "principal,suraj");
+        properties.put("spring.sleuth.baggage-keys", "principal,suraj");
         MutablePropertySources propertySources = environment.getPropertySources();
-        propertySources.addFirst(new PropertiesPropertySource("surajSleuthPostProcessor",properties));
+        propertySources.addFirst(new PropertiesPropertySource("surajSleuthPostProcessor", properties));
     }
 }
